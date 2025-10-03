4793f00dd2afed57b299cc3a39887ec4bc056a1b
Maha6468 <motherma7633@gmail.com>
2025-10-04 00:00:35 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_1/screen_page/login_In.dart';
 import 'package:project_1/screen_page/sign_In.dart';
+import 'package:project_1/screen_page/update_Password.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
-      home:Sign_In(),
+      home:LoginIn(),
     );
   }
 }
---
+++ lib/screen_page/update_Password.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:project_1/Cus_Wid_For_All/button.dart';
+import 'package:project_1/Cus_Wid_For_All/text_F_Field.dart';
+import 'package:project_1/Cus_Wid_For_All/text_F_Field_with_Icon.dart';
+
+class Update_Password extends StatefulWidget {
+  const Update_Password({super.key});
+
+  @override
+  State<Update_Password> createState() => _Update_PasswordState();
+}
+
+class _Update_PasswordState extends State<Update_Password> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("Update Password",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),)),
+          SizedBox(height: 20,),
+          Text_Form_Field(label: 'Current Password', filled: true,fillColor: Colors.grey[200],),
+          SizedBox(height: 10,),
+          Text_F_Field_With_Icon(label1: 'New Password', filled: true,fillColor: Colors.grey[200],),
+          SizedBox(height: 10,),
+          Text_F_Field_With_Icon(label1: 'Confirm Password', filled: true,fillColor: Colors.grey[200],),
+          SizedBox(height: 20,),
+          Elevate_Button(text: 'Update Now',)
+
+        ],
+      ),
+    );
+  }
+}
