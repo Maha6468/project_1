6d4367a3c16ce263534554c120785d9d9fa211e4
Maha6468 <motherma7633@gmail.com>
2025-10-05 23:12:17 +0600

    last update

--- lib/screen_page/my_Courses.dart
+++ lib/screen_page/my_Courses.dart
@@ -16,33 +16,42 @@ class _My_CoursesState extends State<My_Courses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-           children: [
-             Top_Row(text: 'My Courses',),
-             SizedBox(height: 15,),
-             TextField_Cus(),
-             SizedBox(height: 12,),
-             Second_Row(text: 'Popular Courses', text_second: 'View all',),
-             SizedBox(height: 1,),
-             ListView_Builder(),
-             SizedBox(height: 1,),
-             Second_Row(text: 'Recommended', text_second: 'View all',),
-             ListView_Builder(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Top_Row(text: 'My Courses',),
+                SizedBox(height: 15,),
+                TextField_Cus(),
+                SizedBox(height: 12,),
+                Second_Row(text: 'Popular Courses', text_second: 'View all',),
+                SizedBox(height: 1,),
+                ListView_Builder(),
+                SizedBox(height: 1,),
+                Second_Row(text: 'Recommended', text_second: 'View all',),
+                ListView_Builder(),
 
 
-           ],
-
+              ],
+            ),
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.deepPurple,
+          currentIndex: 0,
           items: [
             BottomNavigationBarItem(icon:Icon(Icons.home),label: "Home"),
             BottomNavigationBarItem(icon:Icon(Icons.book),label: "My Course"),
             BottomNavigationBarItem(icon:Icon(Icons.favorite),label: "Wishlist"),
             BottomNavigationBarItem(icon:Icon(Icons.person),label: "Account"),
-
-
           ]
       ),
     );
   }
 }
+
