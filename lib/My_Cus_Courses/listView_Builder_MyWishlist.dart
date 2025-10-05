63f5e308a7559e5bec7fc2589ef2c78a1ad0a287
Maha6468 <motherma7633@gmail.com>
2025-10-06 01:00:00 +0600

    last update

--- lib/My_Cus_Courses/listView_Builder_MyWishlist.dart
+++ lib/My_Cus_Courses/listView_Builder_MyWishlist.dart
@@ -30,16 +30,42 @@ class _ListView_Builder_MyWishlistState extends State<ListView_Builder_MyWishlis
                   width: 110,height: 120,
                   fit: BoxFit.cover,),
                 SizedBox(width: 7,),
-                Column(
-                  children: [
-                    Text(
-                      "WordPress for Beginners Master......",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 15,),
+                      Text(
+                        "WordPress for Beginners Master",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5,),
+                      Row(
+                        children: [
+                          Icon(Icons.star,color: Colors.orange,size: 16,),
+                          Text("5.0 (30 Reviews)")
+                        ],
+                      ),
+                      SizedBox(height: 5,),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                          child: Text("\$15.00",style: TextStyle(fontWeight: FontWeight.bold),))
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                   // padding: EdgeInsets.all(8),
+                    //margin: EdgeInsets.only(left: 8),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey.shade200,
                     ),
-                  ],
-                )
+                    child: const Icon(Icons.remove, color: Colors.black),
+                  ),
+                ),
 
               ],
             ),
