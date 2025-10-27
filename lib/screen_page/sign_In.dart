ddfef633f2b1ebd2cc9f4fd9c67e15c35ae3bcfe
Maha6468 <motherma7633@gmail.com>
2025-10-28 01:36:23 +0600

    last update

--- lib/screen_page/sign_In.dart
+++ lib/screen_page/sign_In.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_1/Custom_Widget_For_All/top_arrow_icon.dart';
 
 import '../Custom_Widget_For_All/elevated_button.dart';
 import '../Custom_Widget_For_All/text_F_Field_with_Icon.dart';
@@ -25,16 +26,17 @@ class _Sign_InState extends State<Sign_In> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                
+                Top_Arrow_Icon(),
+                SizedBox(height: 0),
                 Center(child: Text("Sign In",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),)),
                 SizedBox(height: 10,),
-                Text_Form_Field(label: 'First name', filled: true,fillColor: Colors.grey[200],),
+                Text_Form_Field(lablText: 'First name', filled: true,fillColor: Colors.grey[200],),
                 SizedBox(height: 10,),
-                Text_Form_Field(label: 'Last name', filled: true,fillColor: Colors.grey[200],),
+                Text_Form_Field(lablText: 'Last name', filled: true,fillColor: Colors.grey[200],),
                 SizedBox(height: 10,),
-                Text_Form_Field(label: 'Email', filled: true,fillColor: Colors.grey[200],),
+                Text_Form_Field(lablText: 'Email', filled: true,fillColor: Colors.grey[200],),
                 SizedBox(height: 10,),
-                Text_F_Field_With_Icon(hintText: 'Password',),
+                Text_Form_Field(lablText: 'Password', filled: true,fillColor: Colors.grey[200],sufIcon: Icons.visibility_off_outlined,),
                 SizedBox(height: 10,),
                 Elevated_Button(text: 'Sign In',width: 320,height: 40,onPressed: (){},),
                 SizedBox(height: 5,),
