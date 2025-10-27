224e0b031c66a30f7e83de5dfa754a1fb3167a10
Maha6468 <motherma7633@gmail.com>
2025-10-28 01:36:34 +0600

    last update

---
+++ lib/Custom_Widget_For_All/top_Row.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class Top_Row extends StatelessWidget {
+  final String text;
+  const Top_Row({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(7),
+            ),
+            child: Icon(Icons.arrow_back_ios_new,size: 25,)),
+        Text(text,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+        Icon(Icons.more_vert)
+
+      ],
+    );
+  }
+}
