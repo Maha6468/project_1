fedd66a0af7fabcb95a3f5b863be444329105b52
Maha6468 <motherma7633@gmail.com>
2025-10-28 01:35:02 +0600

    last update

--- lib/screen_page/login_In.dart
+++ lib/screen_page/login_In.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:project_1/Custom_Widget_For_All/top_arrow_icon.dart';
 import 'package:project_1/screen_page/sign_In.dart';
 import 'package:project_1/screen_page/update_Password.dart';
 import '../Custom_Widget_For_All/elevated_button.dart';
@@ -7,15 +8,14 @@ import '../Custom_Widget_For_All/text_form_field.dart';
 import 'main_Screen.dart';
 
 class LoginIn extends StatefulWidget {
-
-   LoginIn({super.key});
+  LoginIn({super.key});
 
   @override
   State<LoginIn> createState() => _LoginInState();
 }
 
 class _LoginInState extends State<LoginIn> {
-  bool _isChecked=true;
+  bool _isChecked = true;
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +25,28 @@ class _LoginInState extends State<LoginIn> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Center(child: Text("Log In",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
-              SizedBox(height: 15,),
-              Text_Form_Field(label: "Email",filled: true,fillColor: Colors.grey[200],),
-              SizedBox(height: 10,),
-              Text_Form_Field(label: "Password",filled: true,fillColor: Colors.grey[200],sufIcon: Icons.visibility_off_outlined,),
-              SizedBox(height: 1,),
+             Top_Arrow_Icon(),
+              SizedBox(height: 50,),
+              Center(
+                child: Text(
+                  "Log In",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text_Form_Field(
+                lablText: "Email",
+                filled: true,
+                fillColor: Colors.grey[200],
+              ),
+              SizedBox(height: 10),
+              Text_Form_Field(
+                lablText: "Password",
+                filled: true,
+                fillColor: Colors.grey[200],
+                sufIcon: Icons.visibility_off_outlined,
+              ),
+              SizedBox(height: 1),
 
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,52 +54,81 @@ class _LoginInState extends State<LoginIn> {
                   Row(
                     children: [
                       Checkbox(
-                          value: _isChecked,
-                          onChanged: (value){
-                            setState(() {
-                              _isChecked=value!;
-                            });
-                          }),
+                        value: _isChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            _isChecked = value!;
+                          });
+                        },
+                      ),
                       Text("Remember me"),
                     ],
                   ),
-                  TextButton(onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(builder: (context)=>Update_Password()));
-                  }, child: Text(
-                    "Forgot password",
-                    style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.underline
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Update_Password(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Forgot password",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        decoration: TextDecoration.underline,
+                      ),
                     ),
-                  ))
+                  ),
                 ],
               ),
 
-              Elevated_Button(text: "Log In",width: 320,height: 50,
-                onPressed:(){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=>MainScreen()));
-                },),
-              SizedBox(height: 10,),
+              Elevated_Button(
+                text: "Log In",
+                width: 320,
+                height: 50,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainScreen()),
+                  );
+                },
+              ),
+              SizedBox(height: 10),
               SizedBox(
-                width: 320,height: 50,
+                width: 320,
+                height: 50,
                 child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape:RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)
-                        ),
-                        side: BorderSide(color: Colors.black12)
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                    onPressed: (){
-                      Navigator.push(context,MaterialPageRoute(builder: (context)=>Sign_In()));
-                    }, child:Text("Register",style: TextStyle(color: Colors.black),)),
+                    side: BorderSide(color: Colors.black12),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Sign_In()),
+                    );
+                  },
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
               ),
 
-              SizedBox(height: 30,),
-              Text("Or continue with",style: TextStyle(
+              SizedBox(height: 30),
+              Text(
+                "Or continue with",
+                style: TextStyle(
                   color: Colors.black,
-                  fontWeight: FontWeight.bold),),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
 
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Row(
@@ -96,17 +141,22 @@ class _LoginInState extends State<LoginIn> {
                           padding: EdgeInsets.symmetric(vertical: 12),
                           backgroundColor: Colors.white,
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)
-                          )
+                            borderRadius: BorderRadius.circular(16),
+                          ),
                         ),
                         child: Row(
                           mainAxisSize: MainAxisSize.min,
                           children: [
-                            Image.asset("assets/images/google.png",fit:BoxFit.cover,height: 28,),
-                            SizedBox(width: 10,),
+                            Image.asset(
+                              "assets/images/google.png",
+                              fit: BoxFit.cover,
+                              height: 28,
+                            ),
+                            SizedBox(width: 10),
                             Flexible(
                               child: Text(
-                                "Google",style: TextStyle(color: Colors.black),
+                                "Google",
+                                style: TextStyle(color: Colors.black),
                                 overflow: TextOverflow.ellipsis,
                               ),
                             ),
@@ -115,25 +165,30 @@ class _LoginInState extends State<LoginIn> {
                       ),
                     ),
                     SizedBox(width: 10),
-                   Expanded(
+                    Expanded(
                       child: OutlinedButton(
                         onPressed: () {},
                         style: OutlinedButton.styleFrom(
-                            side: BorderSide.none,
-                            padding: EdgeInsets.symmetric(vertical: 12),
-                            backgroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16)
-                            )
+                          side: BorderSide.none,
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
                         ),
                         child: Row(
                           mainAxisSize: MainAxisSize.min,
                           children: [
-                            Image.asset("assets/images/facebook.png",fit:BoxFit.cover,height: 28,),
-                            SizedBox(width: 10,),
+                            Image.asset(
+                              "assets/images/facebook.png",
+                              fit: BoxFit.cover,
+                              height: 28,
+                            ),
+                            SizedBox(width: 10),
                             Flexible(
                               child: Text(
-                                "Facebook",style: TextStyle(color: Colors.black),
+                                "Facebook",
+                                style: TextStyle(color: Colors.black),
                                 overflow: TextOverflow.ellipsis,
                               ),
                             ),
@@ -144,22 +199,25 @@ class _LoginInState extends State<LoginIn> {
                   ],
                 ),
               ),
-              SizedBox(height: 6,),
+              SizedBox(height: 6),
               Text.rich(
-                  TextSpan(
-                      text:"Not have an account yet?",
-                      children: [
-                        TextSpan(
-                            text: "Sign up",style: TextStyle(color: Colors.deepPurple,fontSize:20 ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap=(){
-                                Navigator.push(context,MaterialPageRoute(builder: (context)=>Sign_In()));
-                              }
-                        )
-                      ]
-
-                  )
-              )
+                TextSpan(
+                  text: "Not have an account yet?",
+                  children: [
+                    TextSpan(
+                      text: "Sign up",
+                      style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Sign_In()),
+                          );
+                        },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
