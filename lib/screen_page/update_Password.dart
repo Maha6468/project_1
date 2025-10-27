a4de58e9279fc4eb7bb87629e148970e4d0147e3
Maha6468 <motherma7633@gmail.com>
2025-10-28 01:35:15 +0600

    last update

--- lib/screen_page/update_Password.dart
+++ lib/screen_page/update_Password.dart
@@ -23,13 +23,13 @@ class _Update_PasswordState extends State<Update_Password> {
         children: [
           Center(child: Text("Update Password",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),)),
           SizedBox(height: 20,),
-          Text_Form_Field(label: 'Current Password', filled: true,fillColor: Colors.grey[200],),
+          Text_Form_Field(lablText: 'Current Password', filled: true,fillColor: Colors.grey[200],),
           SizedBox(height: 10,),
-          Text_F_Field_With_Icon(hintText: 'New Password',),
+          Text_Form_Field(lablText: 'New Password', filled: true,fillColor: Colors.grey[200],sufIcon: Icons.visibility_off_outlined,),
           SizedBox(height: 10,),
-          Text_F_Field_With_Icon(hintText: 'Confirm Password',),
+          Text_Form_Field(lablText: 'Confirm Password', filled: true,fillColor: Colors.grey[200],sufIcon: Icons.visibility_off_outlined,),
           SizedBox(height: 20,),
-          Elevated_Button(text: 'Update Now',width: 320,height: 40,onPressed: (){},)
+          Elevated_Button(text: 'Update Now',width: 320,height: 50,onPressed: (){},)
 
         ],
       ),
