28542d08bc05cf1baf0ca136d8c78ac07ea23c4e
Maha6468 <motherma7633@gmail.com>
2025-11-23 13:36:12 +0600

    last update

--- lib/screen_page/filter_Courses.dart
+++ lib/screen_page/filter_Courses.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../Custom_Widget_For_All/elevated_button.dart';
 import '../Custom_Widget_For_All/cus_Text.dart';
 import '../Custom_Widget_For_All/custom_Menu_Icon.dart';
-import '../Custom_Widget_For_All/text_F_Field_with_Icon.dart';
+import '../Custom_Widget_For_All/text_form_field.dart';
 import '../Custom_Widget_For_All/top_Row.dart';
 
 class Filter_Courses extends StatefulWidget {
@@ -29,10 +29,7 @@ class _Filter_CoursesState extends State<Filter_Courses> {
                   children: [
                     Top_Row(text: 'Filter Courses'),
                     SizedBox(height: 20),
-                    Text_F_Field_With_Icon(
-                      hintText: "Search",
-                      prefixIcon: Icon(Icons.search),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "Search",preIcon: Icons.search,),
                     SizedBox(height: 12),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,50 +45,35 @@ class _Filter_CoursesState extends State<Filter_Courses> {
                       ],
                     ),
                     // SizedBox(height: 12,),
-                    Text_F_Field_With_Icon(
-                      hintText: "All category",
-                      suffixIcon: Icon(Icons.keyboard_arrow_down),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "All category",sufIcon: Icons.keyboard_arrow_down,),
                     // SizedBox(height: 12,),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Cus_Text(text: 'Sub Category'),
                     ),
                     //SizedBox(height: 12,),
-                    Text_F_Field_With_Icon(
-                      hintText: "All sub category",
-                      suffixIcon: Icon(Icons.keyboard_arrow_down),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "All sub category",sufIcon: Icons.keyboard_arrow_down,),
                     //SizedBox(height: 12,),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Cus_Text(text: 'Pricing'),
                     ),
                     //SizedBox(height: 12,),
-                    Text_F_Field_With_Icon(
-                      hintText: "All price",
-                      suffixIcon: Icon(Icons.keyboard_arrow_down),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "All price",sufIcon: Icons.keyboard_arrow_down,),
                     // SizedBox(height: 12,),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Cus_Text(text: 'Level'),
                     ),
                     // SizedBox(height: 12,),
-                    Text_F_Field_With_Icon(
-                      hintText: "All level",
-                      suffixIcon: Icon(Icons.keyboard_arrow_down),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "All level",sufIcon: Icons.keyboard_arrow_down,),
                     // SizedBox(height: 12,),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Cus_Text(text: 'Language'),
                     ),
                     // SizedBox(height: 12,),
-                    Text_F_Field_With_Icon(
-                      hintText: "All language",
-                      suffixIcon: Icon(Icons.keyboard_arrow_down),
-                    ),
+                    Text_Form_Field(filled: true,fillColor:Colors.grey[200],hinText: "All language",sufIcon: Icons.keyboard_arrow_down,),
                     SizedBox(height: 16,),
                     Elevated_Button(text: 'Filter',width: 100,onPressed: (){},)
                   ],
